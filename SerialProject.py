a30b3d81562636c76488667eca2a77ef16836460
And <aate.or@gmail.com>
2017-10-08 14:18:36 -0500

    implementación kmeans y cosine serial

--- SerialProject.py
+++ SerialProject.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from vectorizer import cluster_paragraphs
 import os
 
 #Parte para ver todos los archivos tipo txt y guardarlos en una lista
@@ -15,6 +14,7 @@ for f in files:
 		filecontent.append(f)
 for x in filecontent:
 	print x
+print '\n\n\n'
 #Vamos a ver como pasar el contenido del txt para poder analizarlo
 
 for f in filecontent:
@@ -23,10 +23,13 @@ for f in filecontent:
 		data=myfile.read().replace('\n', '')
 		dataset.append(data)
 #Luego de obtener el texto de cada documento se pasa a un vector con el cual analizaremos la similaridad
+num_clusters =3
+cluster_paragraphs(dataset, num_clusters,filecontent)
+clusters = cluster_paragraphs(dataset, num_clusters,filecontent)
 
-vec = TfidfVectorizer()
-parameters = vec.fit_transform(dataset)
-
-result = cosine_similarity(parameters)
-#Se imprimne el resultado
-print result
+cont = 0
+for group in clusters:
+	print('Group {0}\n'.format(cont))
+	print '\n'.join(t for t in clusters[cont])
+	cont = cont + 1
+print '\n\n\n'
