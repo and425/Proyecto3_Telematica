6323d52eee501ad11e7596e18812637201939539
And <aate.or@gmail.com>
2017-10-10 15:59:15 -0500

    commit

--- kmeansSKlearn.py
+++ kmeansSKlearn.py
@@ -40,3 +40,132 @@ for i in range(true_k):
  
 print("\n")
 print("The execution time was %s seconds" % (time.time() - start_time))
+
+
+
+#------------------------------------------------------------
+# A partir de aquí kmeans que imprime el nombre de los archivos
+
+"""
+
+from __future__ import print_function
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+from sklearn.datasets import load_files
+import time
+import os
+import nltk
+import re
+import pandas as pd
+from sklearn.externals import joblib
+
+from nltk.stem.snowball import SnowballStemmer
+
+start_time = time.time()
+dataset =[]
+filecontent = []
+path ='./txt'
+files = [f for f in os.listdir(path) if os.path.split(f)]
+for f in files:
+	if f.endswith(".txt"):
+		filecontent.append(f)
+#Vamos a ver como pasar el contenido del txt para poder analizarlo
+
+for f in filecontent:
+	j = os.path.join(path, f)
+	with open(j, 'r') as myfile:
+		data=myfile.read().decode('utf8')
+		dataset.append(data)
+
+stopwords = nltk.corpus.stopwords.words('english')
+stemmer = SnowballStemmer("english")
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
+
+
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    return filtered_tokens
+
+totalvocab_stemmed = []
+totalvocab_tokenized = []
+
+for i in dataset:
+    allwords_stemmed = tokenize_and_stem(i) #for each item in 'synopses', tokenize/stem
+    totalvocab_stemmed.extend(allwords_stemmed) #extend the 'totalvocab_stemmed' list
+    
+    allwords_tokenized = tokenize_only(i)
+    totalvocab_tokenized.extend(allwords_tokenized)
+
+vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index = totalvocab_stemmed)
+print ("there are ", str(vocab_frame.shape[0]) , " items in vocab_frame ")
+
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
+                                 min_df=0.2, stop_words='english',
+                                 use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1,3))
+
+tfidf_matrix = tfidf_vectorizer.fit_transform(dataset) #fit the vectorizer to synopses
+
+print(tfidf_matrix.shape)
+
+terms = tfidf_vectorizer.get_feature_names()
+
+num_clusters = 2
+
+km = KMeans(n_clusters=num_clusters)
+
+km.fit(tfidf_matrix)
+
+clusters = km.labels_.tolist()
+
+joblib.dump(km,  'doc_cluster.pkl')
+km = joblib.load('doc_cluster.pkl')
+clusters = km.labels_.tolist()
+films = { 'title': filecontent, 'synopsis': dataset, 'cluster': clusters}
+
+frame = pd.DataFrame(films, index = [clusters] , columns = ['rank', 'title', 'cluster', 'genre'])
+grouped = frame['rank'].groupby(frame['cluster'])
+
+
+
+print("Top terms per cluster:")
+print()
+#sort cluster centers by proximity to centroid
+order_centroids = km.cluster_centers_.argsort()[:, ::-1] 
+
+for i in range(num_clusters):
+    print("Cluster %d words:" % i, end='')
+    
+    for ind in order_centroids[i, :6]: #replace 6 with n words per cluster
+        print(' %s' % vocab_frame.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'), end=',')
+    print() #add whitespace
+    print() #add whitespace
+    
+    print("Cluster %d titles:" % i, end='')
+    for filecontent in frame.ix[i]['title'].values.tolist():
+        print(' %s,' % filecontent, end='')
+    print() #add whitespace
+    print() #add whitespace
+    
+print()
+print()
+ 
+print("\n")
+print("The execution time was %s seconds" % (time.time() - start_time))
+
+"""
