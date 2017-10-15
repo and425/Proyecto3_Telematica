e4373e370471c7bb3395708ac9f7c4755850f673
And <aate.or@gmail.com>
2017-10-14 19:59:36 -0500

    se agrega clustering.sh

---
+++ clustering.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+usage()
+{
+    echo "Help"
+    echo "\t-h | --help"
+    echo "\t-s | --serial"
+    echo "\t-p | --parallel  Number of nodes"
+    echo ""
+}
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $2 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -s | --serial)
+            python ./SerialProject.py
+            exit
+            ;;
+        -p | --parallel)
+            NODES="$2"
+            shift
+            mpiexec -np $NODES python ./ParallelProject.py
+            exit
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
